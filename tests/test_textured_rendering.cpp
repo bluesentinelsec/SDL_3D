c4b770527f0f36e5f93acd6f80afdddeb612a11f
bluesentinelsec <bluesentinel@protonmail.com>
2026-04-18 21:32:30 -0400

    Fix test include formatting

--- tests/test_textured_rendering.cpp
+++ tests/test_textured_rendering.cpp
@@ -7,8 +7,8 @@
 #include <gtest/gtest.h>
 
 #define SDL_MAIN_HANDLED
-#include <SDL3/SDL_filesystem.h>
 #include <SDL3/SDL.h>
+#include <SDL3/SDL_filesystem.h>
 #include <SDL3/SDL_main.h>
 
 extern "C"
