5599b551d0795b0b2edf775656ef835a7c74a06c
bluesentinelsec <bluesentinel@protonmail.com>
2026-05-08 22:01:52 -0400

    Remove native Doom surveillance glue

--- demos/doom_level/renderer.h
+++ demos/doom_level/renderer.h
@@ -11,7 +11,6 @@
 #include "hazard_effects.h"
 #include "level_data.h"
 #include "player.h"
-#include "surveillance.h"
 
 #include <stdbool.h>
 
@@ -30,7 +29,7 @@ void render_state_free(render_state *rs);
 /* Draw one complete frame. Presentation is owned by the managed game loop. */
 void render_draw_frame(render_state *rs, sdl3d_render_context *ctx, const sdl3d_font *font, sdl3d_ui_context *ui,
                        level_data *ld, entities *ent, const doom_hazard_particles *hazards, const doom_doors *doors,
-                       const doom_surveillance_camera *surveillance, const player_state *player, int backbuffer_w,
-                       int backbuffer_h, float dt, const char *render_profile_name);
+                       const player_state *player, int backbuffer_w, int backbuffer_h, float dt,
+                       const char *render_profile_name);
 
 #endif
