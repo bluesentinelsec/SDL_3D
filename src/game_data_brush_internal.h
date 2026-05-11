2a6098d0b2b33e89ce611a26a843b09fe7d78c1a
bluesentinelsec <bluesentinel@protonmail.com>
2026-05-10 21:04:53 -0400

    Refactor brush render model compilation

--- src/game_data_brush_internal.h
+++ src/game_data_brush_internal.h
@@ -17,6 +17,8 @@ bool slayer3d_game_data_brush_world_trace_local(const slayer3d_game_data_brush_w
                                                 const slayer3d_game_data_brush_trace_desc *desc,
                                                 slayer3d_game_data_brush_trace_result *out_result);
 
+bool slayer3d_game_data_brush_world_compile_render_model(slayer3d_game_data_brush_world *world, slayer3d_model *model);
+
 bool slayer3d_game_data_brush_slide_with_trace(slayer3d_game_data_brush_trace_fn trace_fn, void *trace_userdata,
                                                const slayer3d_game_data_brush_trace_desc *desc, int max_bumps,
                                                slayer3d_game_data_brush_trace_result *out_result);
