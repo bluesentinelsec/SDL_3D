daf02e6469f3c26e7dd948587062e81a199a775d
bluesentinelsec <bluesentinel@protonmail.com>
2026-05-25 19:48:14 -0400

    Split actor pool internal types

--- src/game_data_internal.h
+++ src/game_data_internal.h
@@ -7,6 +7,7 @@
 #include <SDL3/SDL_scancode.h>
 #include <SDL3/SDL_stdinc.h>
 
+#include "game_data_actor_pool_types.h"
 #include "game_data_brush_internal.h"
 #include "script_internal.h"
 #include "slayer3d/actor_controller.h"
@@ -463,54 +464,6 @@ typedef struct game_data_input_value
     int action_id;
     float value;
 } game_data_input_value;
-
-typedef enum actor_pool_exhaustion_policy
-{
-    ACTOR_POOL_EXHAUST_FAIL,
-    ACTOR_POOL_EXHAUST_REUSE_OLDEST,
-} actor_pool_exhaustion_policy;
-
-typedef enum actor_pool_scene_exit_policy
-{
-    ACTOR_POOL_SCENE_EXIT_RESET,
-    ACTOR_POOL_SCENE_EXIT_DESPAWN,
-    ACTOR_POOL_SCENE_EXIT_PRESERVE,
-} actor_pool_scene_exit_policy;
-
-typedef enum actor_lifecycle_state
-{
-    ACTOR_LIFECYCLE_INACTIVE,
-    ACTOR_LIFECYCLE_SPAWNING,
-    ACTOR_LIFECYCLE_ACTIVE,
-    ACTOR_LIFECYCLE_DESPAWNING,
-} actor_lifecycle_state;
-
-typedef struct actor_pool_runtime
-{
-    char *name;
-    const char *archetype;
-    const char *scene;
-    const char **scenes;
-    int scene_count;
-    yyjson_val *archetype_json;
-    char **actor_names;
-    Uint64 *spawn_generations;
-    actor_lifecycle_state *lifecycle_states;
-    Uint64 spawn_generation_counter;
-    Uint64 spawn_attempt_count;
-    Uint64 spawn_success_count;
-    Uint64 spawn_failure_count;
-    Uint64 exhaustion_count;
-    Uint64 reuse_count;
-    Uint64 despawn_count;
-    int peak_active_count;
-    char last_spawn_failure_reason[64];
-    char last_despawn_reason[64];
-    int capacity;
-    bool initial_active;
-    actor_pool_exhaustion_policy exhaustion;
-    actor_pool_scene_exit_policy scene_exit_policy;
-} actor_pool_runtime;
 
 typedef struct runtime_direct_connect_session
 {
