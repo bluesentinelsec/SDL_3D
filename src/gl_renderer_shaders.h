288907a55545f50e939d89d8c0a747f37fa3af97
bluesentinelsec <bluesentinel@protonmail.com>
2026-05-25 18:20:06 -0400

    Split OpenGL shader sources

---
+++ src/gl_renderer_shaders.h
@@ -0,0 +1,31 @@
+#ifndef SLAYER3D_GL_RENDERER_SHADERS_H
+#define SLAYER3D_GL_RENDERER_SHADERS_H
+
+extern const char k_pbr_vert[];
+extern const char k_pbr_frag_decl[];
+extern const char k_pbr_frag_main[];
+extern const char k_pbr_frag_shadow_post[];
+extern const char k_pbr_frag_light_post[];
+extern const char k_unlit_vert[];
+extern const char k_unlit_frag[];
+extern const char k_fullscreen_vert[];
+extern const char k_copy_frag[];
+extern const char k_transition_frag[];
+extern const char k_shadow_vert[];
+extern const char k_shadow_frag[];
+extern const char k_point_shadow_vert[];
+extern const char k_point_shadow_frag[];
+extern const char k_bloom_threshold_frag[];
+extern const char k_blur_frag[];
+extern const char k_composite_frag[];
+extern const char k_retro_frag_helpers[];
+extern const char k_retro_frag_main[];
+extern const char k_ssao_frag[];
+extern const char k_cube_vert[];
+extern const char k_equirect_to_cube_frag[];
+extern const char k_irradiance_frag[];
+extern const char k_prefilter_frag[];
+extern const char k_brdf_vert[];
+extern const char k_brdf_frag[];
+
+#endif
